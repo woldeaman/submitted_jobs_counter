bd74e510441fa0cf35926c94879a2db0cbeb0f39
Amanuel Wolde-Kidan <woldeaman@hiraiwamac.physik.fu-berlin.de>
2018-08-29 11:26:09 +0200

    added message if no results files were found

--- watch_jobs/watch_jobs.py
+++ watch_jobs/watch_jobs.py
@@ -7,17 +7,22 @@ import os
 
 
 def main():
+    found_results = False
     # find all sub-directories of current directory
     dirs = glob.glob(os.getcwd()+'/*')
     for d in dirs:
         # only look at directories with results.h5 files in them
         contents = glob.glob(d+'/*')
         if 'results.h5' in ''.join(contents).split('/'):
+            found_results = True
             sp.call(['cp', d+'/results.h5', '.'], cwd=os.getcwd())
             res = pd.HDFStore('results.h5')
             print("%s completed %i iterations" % (d.split('/')[-1], res.root._v_nchildren))
             res.close()
             sp.call(['rm', 'results.h5'], cwd=os.getcwd())
+
+    if not found_results:
+        print("No 'results.h5' file was found in sub-directories...")
 
 
 if __name__ == "__main__":
